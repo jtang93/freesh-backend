9815d5c0717e181ad357171583fb077f99be9696
jtang93 <tang.jonathan93@gmail.com>
2018-12-20 19:48:10 -0500

    mvp

---
+++ app/controllers/api/v1/listings_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::ListingsController < ApplicationController
+
+  skip_before_action :authorized, only: %i[create index]
+
+  def index
+    @listings = Listing.all
+    render json: @listings
+  end
+
+  def create
+    @listing = Listing.create(user_params)
+    if @listing.valid?
+      @token = encode_token(user_id: @listing.id)
+      render json: { listing: UserSerializer.new(@listing) }, status: :created
+    else
+      render json: { error: 'failed to create listing' }, status: :not_acceptable
+    end
+  end
+
+  private
+  def user_params
+
+    params.require(:listing).permit(:user_id, :item, :condition, :location, :picture)
+
+  end
+
+end
